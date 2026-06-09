549e84e5e9198317ebc02b4bf2357e1d0553e672
sravann <sravanbasis55@gmail.com>
2026-06-09 15:16:00 +0530

    mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -6,6 +6,7 @@ sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
+USERID=$(id -u)
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
  R="\e[31m"
  G="\e[32m"
