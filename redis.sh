aea03fcb7f417cd53cde66f9d2f24c3d8c110eb6
sravann <sravanbasis55@gmail.com>
2026-06-11 16:08:22 +0530

    roboshop.sh

--- redis.sh
+++ redis.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-LOGS_FOLDER="var/log/roboshop"
+LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 USERID=$(id -u)
-timestamp=$(date "+%Y-%m-%d %H:%M:%S")
- R="\e[31m"
- G="\e[32m"
- Y="\e[33m"
- N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
- if [ $USERID -ne 0 ]; then
-    echo -e "$timestamp $R please run the script with root access $N" | tee -a $LOGS_FILE
-    exit 1 
- fi
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
 
- validate() {
-     if [ $1 -ne 0 ]; then
-        echo -e "$timestamp $2... $R failure $N" | tee -a $LOGS_FILE
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
-     else 
-        echo -e "$timestamp $2...$G success $N" | tee -a $LOGS_FILE
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
- }
+}
 
-    dnf module disable redis -y &>> $LOGS_FILE
-    dnf module enable redis:7 -y &>> $LOGS_FILE
-    dnf install redis -y &>> $LOGS_FILE
-    validate $? "installing redis:7"
-
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected mode/ c protected mode no' /etc/redis/redis.conf
-validate $? "allowing remote connections"
+dnf module disable redis -y &>> $LOGS_FILE
+dnf module enable redis:7 -y &>> $LOGS_FILE
+dnf install redis -y &>> $LOGS_FILE
+VALIDATE $? "Installing Redis:7"
+ 
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
 
 systemctl enable redis &>> $LOGS_FILE
 systemctl start redis &>> $LOGS_FILE
-validate $? "started redis"
-
+VALIDATE $? "Started Redis"
